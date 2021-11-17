3e9e813669f2f1cb630bc8179d0a73088ee9b557
QxEcR <qxecrx1110@gmail.com>
2021-11-17 13:25:42 +0300

    configure notification

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
     View,
     StyleSheet,
 } from 'react-native';
 
 import BluetoothContainer from './components/BluetoothContainer';
+import PushNotification from "react-native-push-notification";
+
 
 
 export default App = () => {
+  // use effect to run functions on the start of the program
+  useEffect(() => {
+    creatChannel()
+  }, [])
   
+  const creatChannel = () => (
+    PushNotification.createChannel({
+      channelId: "BluetoothTrackerChannel",
+      channelName: "BluetoothTrackerNotificationSystem",
+      vibrate: true,
+    })
+  )
 
   return (
     <View style={style.View}>
--- components/BluetoothContainer.js
+++ components/BluetoothContainer.js
@@ -3,16 +3,29 @@ import {
     Text,
     View,
     StyleSheet,
+    Button,
 } from 'react-native';
 import BluetoothDevice from './BluetoothDevice';
+import PushNotification from "react-native-push-notification";
 
 
 
 const BluetoothContainer = () => {
 
+  const sendNotification = (Title, Message) => {
+    PushNotification.localNotification({
+      channelId: "BluetoothTrackerChannel",
+      title: Title,
+      message: Message
+    })
+  }
+
   return (
       <View>
         <BluetoothDevice BluetoothName={"device HI"} BluetoothState={"OFF"} />
+        <Button title="Test Notification" 
+        onPress={()=>{sendNotification("Hi","this is the body")}}
+        ></Button>
       </View>
     );
 };
